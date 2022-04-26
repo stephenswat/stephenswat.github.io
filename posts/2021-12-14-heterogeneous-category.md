7777712ae58d9ce2f4e4e0666f7a6681290d75a2
Stephen Nicholas Swatman <stephen@svartalfheim.v25.nl>
2022-04-27 01:05:57 +0200

    Update

--- posts/2021-12-14-heterogeneous-category.md
+++ posts/2021-12-14-heterogeneous-category.md
@@ -8,4 +8,6 @@ To the eternal dismay of my colleagues, one of my favourite things to do at
 work is ponder how abstract structures can bring structure to the chaos that is
 heterogeneous computing.
 
+$$\sum_{i = 0}^{120} k^i$$
+
 <img src="/images/heterogeneous_cats.png" style="float: center; width: 100%;" />
--- site.hs
+++ site.hs
@@ -1,7 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.Set
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Text.Pandoc.Options
+
+pandocMathCompiler =
+    let mathExtensions = [Ext_tex_math_dollars, Ext_tex_math_double_backslash,
+                          Ext_latex_macros]
+        defaultExtensions = writerExtensions defaultHakyllWriterOptions
+        newExtensions = foldr enableExtension defaultExtensions mathExtensions
+        writerOptions = defaultHakyllWriterOptions {
+                          writerExtensions = newExtensions,
+                          writerHTMLMathMethod = MathML
+                        }
+    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
 
 main :: IO ()
 main = hakyll $ do
@@ -25,7 +38,7 @@ main = hakyll $ do
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandocMathCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
